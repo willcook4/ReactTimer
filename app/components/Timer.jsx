d5592fbf0da2fd476ef0e86d72116ab25a5fa0b5
Will Cook <hello@doubleucook.com>
2017-02-01 16:09:49 +0000

    test commit for github email

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var Clock = require('Clock');
 var Controls = require('Controls');
 var CountdownForm = require('CountdownForm');
+var test = "test";
 
 var Timer = React.createClass({
   getInitialState: function () {
