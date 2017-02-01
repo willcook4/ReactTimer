10791b722a022fedc6e4539a41de7190138344f2
Will Cook <hello@doubleucook.com>
2017-02-01 15:56:16 +0000

    added a countup timer

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,10 +1,79 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
+var CountdownForm = require('CountdownForm');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if(this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          // console.log('this.state.count',this.state.count);
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function () {
+    // Clearing the timer!
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount <= 9999 ? newCount : 0
+      });
+      if(newCount === 0 || newCount === 9999) {
+        this.setState({countdownStatus: 'stopped'});
+      }
+    }, 1000);
+  },
+  handleSetCountdown: function(seconds){
+    // console.log('seconds: ',seconds);
+    if(isNaN(seconds)){
+      // console.log("not a number")
+      seconds = 0;
+    } else {
+      // console.log('its a number');
+    }
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
   render: function () {
+    var {count, countdownStatus} = this.state;
+
+    var renderControlArea = () => {
+      if(countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+      }
+    };
+
     return (
       <div>
-        <p>This is the Timer</p>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        {renderControlArea()}
       </div>
   )},
 });
